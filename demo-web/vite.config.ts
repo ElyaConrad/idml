6b0b6c5d3b7dd0c2f780982bf735d7b4a9cbcfb8
Maurice Conrad <conr.maur@gmail.com>
2024-11-16 17:39:36 +0100

    well

--- demo-web/vite.config.ts
+++ demo-web/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [nodePolyfills(), vue()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+      define: {
+        global: 'globalThis',
+      },
+      supported: {
+        bigint: true,
+      },
+    },
+  },
+  build: {
+    target: ['esnext'], // 👈 build.target
+    // rollupOptions: {
+    //   plugins: [nodePolyfills()],
+    // },
+  },
+  // resolve: {
+  //   alias: {
+  //     buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+  //     process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+  //   },
+  // },
+});
